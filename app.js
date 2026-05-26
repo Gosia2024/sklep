a73bfeb57e75c94ddecb610543fe311357a33646
Gosia2024 <lm.sprachen@gmail.com>
2026-05-26 14:59:24 +0200

    use discount variable

---
+++ app.js
@@ -0,0 +1,10 @@
+const product1 = { price: 10}
+const product2 = { price: 20}
+const discoutn = 10
+
+let total = product1.price + product2.price
+//total = total - 10
+total += 10
+
+console.log("Cena przed znizka: " + total)
+console.log("Cena po zniszcze " + total)
