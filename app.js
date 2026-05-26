a17988a05942d6799f4daf64bb72d6f64ea11da4
Gosia2024 <lm.sprachen@gmail.com>
2026-05-26 15:15:46 +0200

    use tamplet literals for price output

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const product1 = { price: 10}
-const product2 = { price: 20}
-const discoutn = 10
+const product1 = { price: 10 };
+const product2 = { price: 20 };
+const discoutn = 10;
 
-let total = product1.price + product2.price
+const total = product1.price + product2.price;
 //total = total - 10
-total += 10
+//total += 10;
+const totalWithDiscount = total - 10;
 
-console.log("Cena przed znizka: " + total)
-console.log("Cena po zniszcze " + total)
+console.log(`Cena przed znizka: ${total}
+    Cena po zniszcze ${totalWithDiscount}`);
